aae277d74fa5e68f6127571994361b16a861afee
Pawel Zysk <pawelzysk1989@gmail.com>
2017-06-14 23:34:22 +0200

    another final touch

--- server/router.js
+++ server/router.js
@@ -1,6 +1,9 @@
 
 const contactController = require('./controllers/contactController');
 module.exports = (app) => {
+  app.get('/', function(req, res, next) {
+    res.status(200).send("Hi, It works!")
+  });
   app.post('/addContact', contactController.addContact);
   app.get('/getAllContacts', contactController.getAllContacts);
   app.get('/getContact/:id', contactController.getContact);
